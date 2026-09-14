4ff8c2d1c01b5035b6e7e607947da9846db286ef
makinnillakarn <thhitirat@gmail.com>
2026-09-14 22:04:43 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = 'https://makinnillakarn-kefi-ixg51hi2y-makinnillakarn.vercel.app';
+const APPS_SCRIPT_URL = 'https://docs.google.com/spreadsheets/d/1MD0hsMFgT7o_gEPoUhqHCkairyqYsxTGMq9JLmNjg0o/edit?usp=sharing';
 const CSV_URL = 'YOUR_CSV_URL';
 
 document.addEventListener('DOMContentLoaded', () => {
